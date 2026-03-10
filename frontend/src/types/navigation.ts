d6abbb83424cfbdbeeefb9668fd9b5fed51aa72a
Patryk Kępa <61762509+patkepa@users.noreply.github.com>
2026-03-10 16:49:06 +0100

    chore: update navigation.ts

--- frontend/src/types/navigation.ts
+++ frontend/src/types/navigation.ts
@@ -12,3 +12,15 @@ export interface NavGroup {
   items: NavItem[];
 }
 
+export interface Project {
+  name: string;
+  environment: 'Production' | 'Development' | 'Testing';
+  icon: IconName;
+}
+
+export interface User {
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
